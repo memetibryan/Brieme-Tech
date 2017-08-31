061919abb7ecee8f3de533378f24a42c468c7195
memetibryan <memetibryan@gmail.com>
2017-08-31 16:54:33 +0300

    pagination successfully done

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,7 +1,7 @@
 class TradesController < ApplicationController	
 	def index
-		@trade_items = if params[:term]
-          Credit.where('location LIKE ?', "%#{params[:term]}%")
+		@products = if params[:term]
+          Credit.where('location LIKE ?', "%#{params[:term]}%").paginate(:page => params[:page], :per_page => 2)
         else
           @products = Credit.paginate(:page => params[:page], :per_page => 2)
         end
